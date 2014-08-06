c39f081ebd3e98a5bfd17a7a07faeece336527df
kubota-akira <kubota_akira1@yahoo.co.jp>
2014-08-06 15:23:23 +0900

    first comit

---
+++ Applet_test/src/Test01.java
@@ -0,0 +1,24 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class Test01 extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		g.drawString("•¶Žš‚ð•\Ž¦",10,10);
+	}
+
+}
